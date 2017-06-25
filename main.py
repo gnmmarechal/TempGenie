4a67e3e13d5b8a39f65614133d06082dcb045866
Mário Alexandre Lopes Liberato <gnmpolicemata91@gmail.com>
2017-06-25 22:28:15 +0100

    Added comments

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import random
 import sys
 
 
+# Gets the token from a config file
 def get_token():
     token = []
     with open("token.cfg") as f:
@@ -14,11 +15,13 @@ def get_token():
     return token[0]
 
 
+# Writes text to a file as a new line
 def add_text(text, fname):
     with open(fname, 'a') as file:
         file.writelines("\n" + text)
 
 
+# Checks if a line exists in a file
 def quote_exists(text, fname):
     quotes = []
     with open(fname) as f:
@@ -28,6 +31,7 @@ def quote_exists(text, fname):
     return False
 
 
+# Returns a random non-empty line from a file
 def get_from_list(fname):
     quotes = []
     with open(fname) as f:
@@ -35,6 +39,7 @@ def get_from_list(fname):
     quotes = list(filter(None, quotes))
     return random.choice(quotes)
 
+# Initializes the bot and sets the command prefix
 my_bot = Bot(command_prefix=".")
 
 
@@ -43,31 +48,37 @@ async def on_read():
     print("Client logged in")
 
 
+# Makes the bot say hello
 @my_bot.command()
 async def hello():
     return await my_bot.say(get_from_list("hello.txt"))
 
 
+# Makes the bot reply "GAY"
 @my_bot.command()
 async def ha():
     return await my_bot.say("GAYYYYYYYYYYYYYYYYYYYYYYYYYY!")
 
 
+# Makes the bot reply "Hello" followed by a the arguments
 @my_bot.command()
 async def test(*args):
     return await my_bot.say("Hello " + str(args))
 
 
+# Makes the bot say a random quote
 @my_bot.command()
 async def quote():
     return await my_bot.say(get_from_list("quotes.txt"))
 
 
+# Makes the bot say random trivia
 @my_bot.command()
 async def trivia():
     return await my_bot.say(get_from_list("trivia.txt"))
 
 
+# Makes the bot reply with all available quotes
 @my_bot.command()
 async def all_quotes():
     quotes = []
@@ -79,6 +90,7 @@ async def all_quotes():
         await my_bot.say(quote)
 
 
+# Makes the bot reply with all available trivia
 @my_bot.command()
 async def all_trivia():
     quotes = []
@@ -90,17 +102,20 @@ async def all_trivia():
         await my_bot.say(quote)
 
 
+# Makes the bot reply with its opinion on sleep
 @my_bot.command()
 async def sleep():
     return await my_bot.say("Sleep is for the weak!")
 
 
+# Kills the bot
 @my_bot.command()
 async def kill():
     await my_bot.say(get_from_list("mercy.txt"))
     sys.exit()
 
 
+# Adds a quote to the list
 @my_bot.command()
 async def add_quote(*args):
     final_quote = ""
@@ -115,6 +130,7 @@ async def add_quote(*args):
         await my_bot.say("Quote already exists!")
 
 
+# Adds trivia to the list
 @my_bot.command()
 async def add_trivia(*args):
     final_quote = ""
@@ -128,6 +144,8 @@ async def add_trivia(*args):
     else:
         await my_bot.say("Quote already exists!")
 
+
+# Shows the help page
 @my_bot.command()
 async def man():
     quotes = []
@@ -137,4 +155,5 @@ async def man():
         await my_bot.say(quote)
 
 
+# Runs the bot with the token
 my_bot.run(get_token())
